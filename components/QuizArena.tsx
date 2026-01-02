4c9084f1253e52f79aab154d210fddc85135347c
nickkwork08-jpg <nickk.work.08@gmail.com>
2026-01-02 21:12:05 +0530

    Update QuizArena.tsx

--- components/QuizArena.tsx
+++ components/QuizArena.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { StudyContextType, QuizDifficulty, QuizQuestion } from '../types';
-import { generateQuiz, getMotivationalMessage } from '@/services/geminiService.ts';
+import { generateQuiz, getMotivationalMessage } from '@/services/geminiService';
 import { Play, Loader2, CheckCircle, XCircle, RefreshCcw, Award, ArrowRight, Home, BarChart3, AlertCircle, X } from 'lucide-react';
 
 interface QuizArenaProps {
